2a46c625e0ac130222d1c4cf3b243613ea54d1d5
snake <824475386@qq.com>
2020-03-13 01:06:15 +0800

    手写代理模式

--- proxy/src/main/java/com/snake/proxy/Main.java
+++ proxy/src/main/java/com/snake/proxy/Main.java
@@ -12,5 +12,6 @@ public class Main {
     public static void main(String[] args) {
         SnakeService snakeService = (SnakeService) MyProxy.newInstance(new SnakeServiceImpl());
         snakeService.printStr();
+        snakeService.sayName("陈","威");
     }
 }
